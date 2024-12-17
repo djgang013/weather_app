142b542e096a12d9875da5cfdd3f074ee1de3e16
zakaria <djgang013@gmail.com>
2024-12-17 10:28:27 +0100

    Initial commit

---
+++ Desktop/weather-app/src/App.js
@@ -0,0 +1,12 @@
+import WeatherApp from "./Components/weatherApp/weatherApp";
+function App() {
+  return (
+  <div className="App">
+   <WeatherApp/>
+    </div>
+  );
+}
+
+export default App;
+
+
